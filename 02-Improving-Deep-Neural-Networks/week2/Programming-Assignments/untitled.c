aac2a68fc33ebb6f364536fb3aa9774513f39a28
Richard <acprime@163.com>
2018-03-06 16:53:34 +0800

    initial commit

---
+++ 02-Improving-Deep-Neural-Networks/week2/Programming-Assignments/untitled.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int iLoveYou(char name[]);
+
+int main()
+{
+    iLoveYou("Little Tongtong")
+}
+
+int iLoveYou(char name[])
+{
+    print(name);
+}
+
+
+#include <stdio.h>
+
+int iLoveYou(char name[])
+{
+    print(name);
+}
+
+int main()
+{
+    iLoveYou("Little Tongtong")
+}
